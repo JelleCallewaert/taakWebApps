d2066830b8327baab49a1f9c699009aa723d40d8
Callewaert Jelle <jelle.callewaert.y9088@hogent.student.be>
2018-08-10 14:57:39 +0200

    101 foutjes gefixt (restored)

--- src/app/spel/doelgroep-filter.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { DoelgroepFilterPipe } from './doelgroep-filter.pipe';
-
-describe('DoelgroepFilterPipe', () => {
-  it('create an instance', () => {
-    const pipe = new DoelgroepFilterPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
--- src/app/spel/used-doelgroep-filter.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { UsedDoelgroepFilterPipe } from './used-doelgroep-filter.pipe';
-
-describe('UsedDoelgroepFilterPipe', () => {
-  it('create an instance', () => {
-    const pipe = new UsedDoelgroepFilterPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
